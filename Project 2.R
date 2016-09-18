d70af708cc2143dfdb30a1dc3362db75612eb471
zqfmgbxj <joshyhkim@gmail.com>
2016-09-17 20:31:46 -0400

    Project 2

--- Project 2.R
+++ Project 2.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 titanic <- read.csv("titanic3.csv")
 
 str(titanic)
@@ -10,6 +12,12 @@ titanic$embarked <- sub("^$", "S", titanic$embarked)
   #1. Calculate the mean of the Age column and use that value to populate the missing values
 mean_age <- mean(titanic$age, na.rm = TRUE)
 titanic$age[is.na(titanic$age)] <- mean_age
-titanic$age
 
+#3 Fill boat empty slots with a dummy value e.g. the string 'None' or 'NA'.
+titanic$boat <- sub("^$", NA, titanic$boat)
 
+#4: Cabin - Create a new column has_cabin_number which has 1 if there is a cabin number, and 0 otherwise.
+titanic$cabin <- sub("^$", NA, titanic$cabin)
+titanic <- mutate(titanic, has_cabin_number = ifelse(is.na(titanic$cabin),0,1))
+
+write.csv(titanic, "titanic_clean.csv")
